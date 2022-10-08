ec8a561a2e7d959f024934fd3c6218a40efaf06f
RamazanOzmert <ramazanozmert3@gmail.com>
2022-10-08 15:49:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// const { default: axios } = require("axios");
+import axios from 'axios';
+
+ async function getData(user_id){
+    let linkUsers = "https://jsonplaceholder.typicode.com/users/"+user_id;
+    let linkPosts= "https://jsonplaceholder.typicode.com/posts?userId="+user_id;
+
+    const {data:users} = await axios(linkUsers.toString());
+    const {data:posts} = await axios(linkPosts.toString());
+    
+    return (users,posts);
+};
+
+ export default getData;
+
